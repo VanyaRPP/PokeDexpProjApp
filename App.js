8b5ec40cb52dd2f7c953d7290658bfddd2476e87
Vanya <vanassssssssss@gmail.com>
2021-03-06 21:16:55 +0300

    123

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import styled from 'styled-components'
 import { PokeItem, PokeView } from './components';
 
 import 'react-native-gesture-handler';
+import { Portal } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -12,29 +13,28 @@ const Tab = createBottomTabNavigator();
 function SettingsScreen() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
+      <PokeView
+        url='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
+        color1='#bf2ac9'
+        color2='#3ae831'
+      />
     </View>
   );
 }
 
 const App = () => {
   return (
-    <View>
-      <PokeView
-        url='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
-        color1='#bf2ac9'
-        color2='#3ae831'
-      />
-      <PokeItem />
-      <NavigationContainer>
+    <NavigationContainer>
+      <Portal.Host>
         <Tab.Navigator>
           <Tab.Screen name="Home" component={PokeItem} />
+          <Tab.Screen name="Home1" component={SettingsScreen} />
+          <Tab.Screen name="Home12" component={PokeItem} />
+          <Tab.Screen name="Home31" component={SettingsScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>
-      </NavigationContainer>
-
-    </View> 
-    
+      </Portal.Host>
+    </NavigationContainer>
   );
 };
 
