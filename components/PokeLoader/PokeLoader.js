0ed329d1373319cbac4dea41fe8bb71c951e1bd1
Greifelt228 <68552897+Greifelt228@users.noreply.github.com>
2021-03-23 16:58:07 +0200

    PokeLoaderFunctionalComp

--- components/PokeLoader/PokeLoader.js
+++ components/PokeLoader/PokeLoader.js
@@ -1,53 +1,44 @@
-import React, { Component } from 'react'
-import { AppRegistry,
-  StyleSheet,
-  Text,
-  View,
-  Animated,
-  Image,
-  Easing } from 'react-native'
-import { PokeBallIcon } from '../../assets/images'
+import React, { useEffect, useState, useRef } from 'react'
+import { View, StyleSheet, Animated, Easing } from 'react-native'
+import { PokeBallIcon } from '../../assets/images';
 
-export default class PokeLoader extends Component {
-  constructor () {
-    super()
-    this.spinValue = new Animated.Value(0)
-  }
-  componentDidMount () {
-    this.spin()
-  }
-  spin () {
-    this.spinValue.setValue(0)
-    Animated.timing(
-      this.spinValue,
-      {
-        toValue: 1,
-        duration: 4000,
-        easing: Easing.linear
-      }
-    ).start(() => this.spin())
-  }
-  render() {
-    const spin = this.spinValue.interpolate({
-      inputRange: [0, 1],
-      outputRange: ['0deg', '360deg']
+
+export default function SplashLoading() {
+  const [rotateValue, setRotateValue] = useState(new Animated.Value(0));
+  useEffect(() => {
+    StartImageRotate();
+  }, []);
+
+  function StartImageRotate() {
+    rotateValue.setValue(0);
+
+    Animated.loop(Animated.timing(rotateValue, {
+      toValue: 1,
+      duration: 4000,
+      easing: Easing.linear,
+      useNativeDriver: true,
     })
-    return (
-      <View >
-        <Animated.View
-          style={{
-            transform: [{rotate: spin}] }}
-        >
-          <PokeBallIcon />
-        </Animated.View>
-      </View>
-    )
+    ).start();
   }
+
+  const RotateData = rotateValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
+  return (
+    <View>
+      <Animated.View
+        style={{
+          transform: [{ rotate: RotateData }],
+        }}
+        
+      >
+        <PokeBallIcon />
+      </Animated.View>
+    </View>
+  );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  }
-})
+
+
+
